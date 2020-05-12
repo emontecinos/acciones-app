b2c94ba1dff9d6af9d305ef5b50338d01bc8a678
Eduardo Montecinos <efmontecinos@uc.cl>
2020-05-12 14:43:06 -0400

    Updates

---
+++ precache-manifest.38309c2a421853fd782f12fa4bc6fd19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b044f492c98f096fce32b9111dbb1bb4",
+    "url": "/acciones-app/index.html"
+  },
+  {
+    "revision": "0377be1023c8255eba5b",
+    "url": "/acciones-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "77130872d891c5942c22",
+    "url": "/acciones-app/static/js/2.34212c7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/acciones-app/static/js/2.34212c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0377be1023c8255eba5b",
+    "url": "/acciones-app/static/js/main.56adaa88.chunk.js"
+  },
+  {
+    "revision": "9d0923cc59f2796c2c49",
+    "url": "/acciones-app/static/js/runtime-main.9b54709b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/acciones-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
